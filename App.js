9c32bd7a79d41bbc0db15f8d1f8aab3d0b318690
Gareth Preston <gareth@gpresto.f9.co.uk>
2021-11-08 19:47:25 +0000

    Creating home screen using grid system

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, Image } from 'react-native'
+import { Row, Grid } from "react-native-easy-grid"
+import { NativeBaseProvider, Center, Heading, Input, Button } from 'native-base'
+import pizzaImage from './assets/pizza.jpg'
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Grid>
+        <Row style={styles.row1}>
+          <Center flex={1}>
+            <Image source={ pizzaImage } style={ styles.image } />
+          </Center>
+        </Row>
+        <Row style={styles.row2}>
+          <Center flex={1}>
+            <Heading size="xl">Mario's Pizza</Heading>
+            <Text size="lg">The best pizza in Belfast</Text>
+          </Center>
+        </Row>
+        <Row style={styles.row3}>
+          <Center flex={1}>
+            <Input
+              placeholder='Email address'
+              style={ styles.textBox }
+            />
+            <Button 
+              style={ styles.button } 
+              bg="primary.400"
+              size="md" 
+              block bordered primary>
+              Get updates from Mario's Pizza
+            </Button>
+          </Center>
+        </Row>
+      </Grid>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  row1: {
+    backgroundColor: '#F9423A',
   },
-});
+  row2: {
+    backgroundColor: 'white',
+  },
+  row3: {
+    backgroundColor: '#47894b',
+  },
+  textBox: {
+    backgroundColor: 'white',
+    width: 250,
+    marginBottom: 5,
+  },
+  button: {
+    backgroundColor: '#D3D3D3',
+    width: 250,
+  },
+  image: {
+    width: 405, 
+    height: 259,
+  },
+})
+
+export default App;
